6809891281aae345ae9ca0836c13b3d8dc246807
Charan <charankulal0241@gmail.com>
2024-06-12 20:18:12 +0530

    "Added CommandLineRunner implementation to StudentManagementSystemApplication and modified students.html template"

--- src/main/java/com/charan/student_management_system/StudentManagementSystemApplication.java
+++ src/main/java/com/charan/student_management_system/StudentManagementSystemApplication.java
@@ -1,13 +1,30 @@
 package com.charan.student_management_system;
 
+import com.charan.student_management_system.entity.Student;
+import com.charan.student_management_system.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
 
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+//		Student student1= new Student("Charan","Kulal","charan@gmail.com");
+//		studentRepository.save(student1);
+//
+//		Student student2= new Student("Navaneeth","Kulal","navaneeth@gmail.com");
+//		studentRepository.save(student2);
+
+	}
 }
